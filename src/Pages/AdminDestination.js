bee81439a95e88d0ff762392aa2ccf0d67fb1ba3
ganesan0726 <ganeshrajendran79@gmail.com>
2023-11-28 19:06:57 +0530

    update

--- src/Pages/AdminDestination.js
+++ src/Pages/AdminDestination.js
@@ -1,4 +1,6 @@
-import React, {useState} from 'react'
+import React, {useState,useEffect} from 'react';
+import axios from "axios";
+import '../css/adminStyle.css';
 
 const AdminDestination = () => {
 
@@ -8,12 +10,34 @@ const AdminDestination = () => {
     destinationImage : ""
   })
 
+  const [destinationList, updateDestinationList] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:4000/api/list/destination');
+        updateDestinationList(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   const handleInputField = (event) => {
-    updateDestination({...destination, [event.target.id] : event.target.value })
-  }
+    updateDestination({...destination, [event.target.id] : event.target.value })  }
 
   const AddNewDestination = () =>{
-    console.log(destination)
+    const url = "http://localhost:4000/api/upload/destination";
+
+    axios.post(url, destination)
+      .then((response) => {
+        alert(response.data.message);
+      })
+      .catch((error) => {
+        alert(error.message);
+      })
   }
 
   const uploadImage = (event) => {
@@ -29,7 +53,7 @@ const AdminDestination = () => {
   }
 
   return (
-    <div>
+    <div className="container">
       <h3>Admin Destinations</h3>
       <div>
         <label>Enter Destination Name</label>
@@ -44,6 +68,30 @@ const AdminDestination = () => {
         <input type="file" accept=".png,.jpg,.jpeg" id="destinationImage" onChange={uploadImage} />
       </div>
       <button onClick={() => AddNewDestination()}>Add Destination</button>
+      <table>
+        <thead>
+          <tr>
+            <th>Destination Name</th>
+            <th>Destination Image</th>
+            <th>Destination Count</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            destinationList.map((value, index) => {
+              return(
+                <tr key={index}>
+                  <td>{value.destinationName}</td>
+                  <td>
+                    <img src={value.destinationImage} alt="" width="50" />
+                  </td>
+                  <td>{value.destinationCount}</td>
+                </tr>
+              )
+            })
+          }
+        </tbody>
+      </table>
     </div>
   )
 }
--- src/Pages/AdminHotel.js
+++ src/Pages/AdminHotel.js
@@ -1,4 +1,6 @@
-import React,{useState} from 'react'
+import axios from "axios"
+import React,{useState,useEffect} from 'react';
+import "../css/adminStyle.css"
 
 const AdminHotel = () => {
 
@@ -12,27 +14,41 @@ const AdminHotel = () => {
     hotelImage : "",
   })
 
+  const [listHotels, UpdateListHotels] = useState([]);
+
+  // Fetch Hotels List
+  useEffect(() => {
+    loadListHotels()
+  },[]);
+
+  const loadListHotels = () => {
+    const url = "http://localhost:4000/api/list/hotelLists";
+    axios.get(url)
+    .then((response) => { 
+      UpdateListHotels(response.data);
+      console.log(hotelList);
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+  }
+
   const handleInputFields = (event) => {
     updateHotelLists({...hotelList, [event.target.id] : event.target.value})
   }
 
-  const resetForm = () => {
-    updateHotelLists({
-      hotelName: "",
-      locationName: "",
-      stayCount: "",
-      washRoomCount: "",
-      bedCount: "",
-      price: "",
-      hotelImage: "",
-    });
-  };
   
   const AddHotels = () => {
     console.log(hotelList);
-  
-    // Call the resetForm function to clear the input fields
-    resetForm();
+    const url = "http://localhost:4000/api/upload/hotelLists";
+
+    axios.post(url,hotelList)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
   };
 
   const uploadImage = (event) => {
@@ -47,8 +63,8 @@ const AdminHotel = () => {
   }
 
   return (
-    <div>
-      <h3>Admin Destinations</h3>
+    <div className="container">
+      <h3>Admin Hotel Details</h3>
       <div>
         <label>Enter Hotel Name</label>
         <input type="text" placeholder="Destination Name" id="hotelName" onChange={handleInputFields} />
@@ -79,8 +95,39 @@ const AdminHotel = () => {
         <input type="file" accept=".png,.jpg,.jpeg" id="hotelImage" onChange={uploadImage} />
       </div>
       <button onClick={() => AddHotels()} >Add Hotel</button>
+      <br />
+      <table>
+        <thead>
+          <tr>
+            <th>Hotel Name</th>
+            <th>Location Name</th>
+            <th>Stay count</th>
+            <th>Wash Room Count</th>
+            <th>Bed Count</th>
+            <th>Price</th>
+            <th>Hotel Image</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            listHotels.map((value, index) => {
+              return (
+                <tr key={index}>
+                  <td>{value.hotelName}</td>
+                  <td>{value.locationName}</td>
+                  <td>{value.stayCount}</td>
+                  <td>{value.washRoomCount}</td>
+                  <td>{value.bedCount}</td>
+                  <td>{value.price}</td>
+                  <td><img src={value.hotelImage} alt="hotelImage" width= "50" /> </td>
+                </tr>
+              )
+            })
+          }
+        </tbody>
+      </table>
     </div>
   )
 }
 
-export default AdminHotel
+export default AdminHotel;
